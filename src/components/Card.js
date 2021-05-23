ce20723b7c2d9255bf229275852d765b994abac4
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-05-23 14:46:32 -0400

    add: nav, card grid, card elements, and color theme

---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Card({ image }) {
+  return <img src="./images/eevee.png" className="card"></img>;
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export function NavBar() {
+  return (
+    <div className="navbar">
+      <h1 className="title">Memory Game</h1>
+      <div className="scores">
+        <div className="score">Score: 54</div>
+        <div className="top-score">Top Score: 2</div>
+      </div>
+    </div>
+  );
+}
